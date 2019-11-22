768855da06d562cad20977267e0ce8c2d2711dfb
rkonings <randy@randykonings.nl>
2019-11-22 10:18:26 +0100

    add pages

---
+++ src/components/pages/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navigation from '../Navigation';
+
+export default () => {
+	return (
+		<React.Fragment>
+			<Navigation />
+			<div>Login page</div>
+		</React.Fragment>
+	);
+};
---
+++ src/components/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navigation from '../Navigation';
+
+export default () => {
+	return (
+		<React.Fragment>
+			<Navigation />
+			<div>Not Found</div>
+		</React.Fragment>
+	);
+};
