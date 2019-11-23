455d9b247c85f7962b668b3269fb0b588b20189c
rkonings <randy@randykonings.nl>
2019-11-23 17:48:38 +0100

    add basic layout

--- src/components/pages/Ciients.tsx
+++ src/components/pages/Ciients.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navigation from '../Navigation';
+import Basic from 'react-ui/build/Layout/Basic';
+
 export default () => {
 	return (
 		<React.Fragment>
-			<Navigation />
-			<div>Clients page</div>
+			<Basic left={<Navigation />}>
+				<h1>Clients</h1>
+			</Basic>
 		</React.Fragment>
 	);
 };
