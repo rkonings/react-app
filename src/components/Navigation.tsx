43cf71af8a7092bf52ec6451803dd1bd9c63b073
rkonings <randy@randykonings.nl>
2019-11-23 17:48:51 +0100

    add navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { Navigation, NavigationItem } from 'react-ui/build/Navigation';
+import { Home, Inbox, Clients, Agenda, ProjectManagement, Invoices, TimeManagement } from 'react-ui/build/Icon';
 
 export default () => {
+
+	const history = useHistory();
 	return (
-		<div>
-			<NavLink to="/">Dashboard</NavLink>
-			<NavLink to="/login">Login</NavLink>
-			<NavLink to="/clients">Clients</NavLink>
-		</div>
+		<Navigation>
+			<NavigationItem onClick={() => history.push('/')} icon={<Home />}>Dashboard</NavigationItem>
+			<NavigationItem onClick={() => history.push('/clients')} icon={<Inbox />}>Inbox</NavigationItem>
+			<NavigationItem onClick={() => history.push('/clients')} icon={<Clients />}>Clients</NavigationItem>
+			<NavigationItem onClick={() => history.push('/clients')} isActive={true} icon={<Agenda />}>Agenda</NavigationItem>
+			<NavigationItem onClick={() => history.push('/clients')} icon={<ProjectManagement />}>Project management</NavigationItem>
+			<NavigationItem onClick={() => history.push('/clients')} icon={<Invoices />}>Invoices</NavigationItem>
+			<NavigationItem onClick={() => history.push('/clients')} icon={<TimeManagement />}>Time management</NavigationItem>
+		</Navigation>
 	);
 };
