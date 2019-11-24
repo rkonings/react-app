af9edd196eb5848da9a11bba2bd8c9473ae5f7d6
rkonings <randy@randykonings.nl>
2019-11-24 11:52:10 +0100

    remove login logic

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,34 +1,9 @@
 import React from 'react';
-import Navigation from '../Navigation';
-
-import { useMutation } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost';
+import Login from '../Login';
 
 export default () => {
 
-	const LOGIN_MUTATION = gql`
-        mutation {
-            login(email: "randy@randykonings.nl", password: "randykonings") {
-                token
-            }
-        }
-	`;
-
-	const [login, { data, loading, error }] = useMutation(LOGIN_MUTATION);
-
-	React.useEffect(() => {
-		login();
-	}, []);
-
-	if (data) {
-		const { token } = data.login;
-		localStorage.setItem('token', token);
-	}
-
 	return (
-		<React.Fragment>
-			<Navigation />
-			<div>Login page</div>
-		</React.Fragment>
+			<Login />
 	);
 };
