56e879d0b528526c4e7d106cc875f82081613568
rkonings <randy@randykonings.nl>
2019-11-24 13:09:39 +0100

    add page title

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -5,7 +5,10 @@ import Basic from 'react-ui/build/Layout/Basic';
 export default () => {
 	return (
 		<React.Fragment>
-			<Basic left={<Navigation />}>
+			<Basic
+				left={<Navigation/>}
+				pageTitle="Dashboard"
+			>
 				<div>Dashboard</div>
 			</Basic>
 		</React.Fragment>
