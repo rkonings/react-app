edb80558a2ea6bdc2b5105bb162c511a773aab91
rkonings <randy@randykonings.nl>
2019-11-24 11:48:43 +0100

    add correct paths to pages

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,12 +10,12 @@ export default () => {
 	return (
 		<Navigation>
 			<NavigationItem onClick={() => history.push('/')} icon={<Home />}>Dashboard</NavigationItem>
-			<NavigationItem onClick={() => history.push('/clients')} icon={<Inbox />}>Inbox</NavigationItem>
+			<NavigationItem onClick={() => history.push('/inbox')} icon={<Inbox />}>Inbox</NavigationItem>
 			<NavigationItem onClick={() => history.push('/clients')} icon={<Clients />}>Clients</NavigationItem>
-			<NavigationItem onClick={() => history.push('/clients')} isActive={true} icon={<Agenda />}>Agenda</NavigationItem>
-			<NavigationItem onClick={() => history.push('/clients')} icon={<ProjectManagement />}>Project management</NavigationItem>
-			<NavigationItem onClick={() => history.push('/clients')} icon={<Invoices />}>Invoices</NavigationItem>
-			<NavigationItem onClick={() => history.push('/clients')} icon={<TimeManagement />}>Time management</NavigationItem>
+			<NavigationItem onClick={() => history.push('/agenda')} isActive={true} icon={<Agenda />}>Agenda</NavigationItem>
+			<NavigationItem onClick={() => history.push('/project-management')} icon={<ProjectManagement />}>Project management</NavigationItem>
+			<NavigationItem onClick={() => history.push('/invoice')} icon={<Invoices />}>Invoices</NavigationItem>
+			<NavigationItem onClick={() => history.push('/time-management')} icon={<TimeManagement />}>Time management</NavigationItem>
 		</Navigation>
 	);
 };
