8cd21b7b66c3f6ece6056f3d06125bd675895eed
rkonings <randy@randykonings.nl>
2019-12-11 11:42:07 +0100

    remove comment

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,3 @@
-// npm link react-ui
-// cd ../../react-ui/react-ui && npm link ../../react-app/react-app/node_modules/react && npm link ../../react-app/react-app/node_modules/react-dom
-
 import React from 'react';
 import defaultTheme from 'react-ui/build/themes/default';
 import ThemeProvider from 'react-ui/build/themes/Provider';
@@ -10,18 +7,18 @@ import Router from './Router';
 import GlobalStyle from 'react-ui/build/GlobalStyle';
 
 class App extends React.PureComponent {
-	render() {
-		return (
-			<ApolloProvider client={ApolloClient}>
-				<ThemeProvider theme={defaultTheme}>
-					<React.Fragment>
-						<GlobalStyle />
-						<Router />
-					</React.Fragment>
-				</ThemeProvider>
-			</ApolloProvider>
-		);
-	}
+    render() {
+        return (
+            <ApolloProvider client={ApolloClient}>
+                <ThemeProvider theme={defaultTheme}>
+                    <React.Fragment>
+                        <GlobalStyle />
+                        <Router />
+                    </React.Fragment>
+                </ThemeProvider>
+            </ApolloProvider>
+        );
+    }
 }
 
 export default App;
