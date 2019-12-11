aba76bf03646287ce17a70fc0cef8a869e667d96
rkonings <randy@randykonings.nl>
2019-12-11 10:36:02 +0100

    add options to hooks

--- src/modules/user/index.ts
+++ src/modules/user/index.ts
@@ -1,4 +1,9 @@
-import { useQuery, useMutation } from '@apollo/react-hooks';
+import {
+    useQuery,
+    useMutation,
+    MutationHookOptions,
+    QueryHookOptions,
+} from '@apollo/react-hooks';
 import getUser from './getUser.graphql';
 import updateUser from './updateUser.graphql';
 export { default as ValidationSchema } from './validationSchema';
@@ -19,10 +24,12 @@ export interface User {
     email: string;
 }
 
-export const useUser = () => {
-    return useQuery(getUser);
+export const useUser = (options?: QueryHookOptions) => {
+    return useQuery(getUser, options);
 };
 
-export const useUpdateUser = () => {
-    return useMutation(updateUser);
+export const useUpdateUser = (
+    options?: MutationHookOptions<any, Record<string, any>>
+) => {
+    return useMutation(updateUser, options);
 };
