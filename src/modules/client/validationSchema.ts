4c9ada5be27f99c481960e1630a59aed0e84cdbe
rkonings <41450820+rkonings@users.noreply.github.com>
2019-12-17 12:44:26 +0100

    feat(client): add regular expression for field validation (#23)

--- src/modules/client/validationSchema.ts
+++ src/modules/client/validationSchema.ts
@@ -3,9 +3,19 @@ import { Yup } from 'react-ui/build/Validation';
 export default Yup.object({
     _id: Yup.string(),
     name: Yup.string().min(1, 'company name is required'),
-    telephone: Yup.string().required('telephone is required'),
-    address: Yup.string().required('password is required'),
+    telephone: Yup.string().matches(
+        /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
+        'Phone number is not valid'
+    ),
+    address: Yup.string()
+        .required('Address is required')
+        .matches(
+            /^([1-9][e][\s])*([a-zA-Z]+(([\.][\s])|([\s]))?)+[1-9][0-9]*(([-][1-9][0-9]*)|([\s]?[a-zA-Z]+))?$/i,
+            'Address is not valid'
+        ),
     city: Yup.string().required('city is required'),
-    zipcode: Yup.string().required('zipcode is required'),
+    zipcode: Yup.string()
+        .required('zipcode is required')
+        .matches(/^[1-9][0-9]{3}[\s]?[A-Za-z]{2}$/i, 'Zipcode is not valid'),
     type: Yup.string().required('type is required'),
 });
