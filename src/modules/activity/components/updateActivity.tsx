aab02af942831b6564074a658f5a22b227d0e795
rkonings <randy@randykonings.nl>
2019-12-29 12:55:51 +0100

    add UpdateActivity component

---
+++ src/modules/activity/components/updateActivity.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import dotProp from 'dot-prop';
+
+import Activity from 'react-ui/build/Activity/UpdateActivity';
+import { ChangedItem, ChangeOptions } from 'react-ui/build/Form';
+
+import { ValidationSchema, useUpdateActivity } from '../../activity';
+import { Activity as ActivityInterface } from 'react-ui/build/Activity/Activity';
+
+interface Activity {
+    activity: ActivityInterface;
+}
+
+export default ({ activity }: Activity) => {
+    const [updateValues] = useUpdateActivity();
+    const onChangeHandler = (
+        items: ChangedItem[],
+        _?: ChangeOptions | undefined,
+        callBack?: (() => void) | undefined
+    ) => {
+        const update = items.reduce((obj, item) => {
+            dotProp.set(obj, item.field, item.value);
+            return obj;
+        }, {});
+
+        updateValues({
+            variables: { activity: { ...update } },
+            update: () => {
+                if (callBack) {
+                    callBack();
+                }
+            },
+        });
+    };
+    return (
+        <Activity
+            validationSchema={ValidationSchema}
+            activity={activity}
+            onChange={onChangeHandler}
+            onRemove={() => null}
+        />
+    );
+};
