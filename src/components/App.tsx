c812951c8db9f61de6b47f1e6eb2ab75ae23a3d4
rkonings <41450820+rkonings@users.noreply.github.com>
2020-01-14 11:25:45 +0100

    feat(portal): add portal provider to App (#39)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,25 @@
+import { ApolloProvider } from '@apollo/react-hooks';
 import React from 'react';
+import GlobalStyle from 'react-ui/build/GlobalStyle';
 import defaultTheme from 'react-ui/build/themes/default';
 import ThemeProvider from 'react-ui/build/themes/Provider';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from './ApolloClient';
-import Router from './Router';
-import GlobalStyle from 'react-ui/build/GlobalStyle';
 import { DataLoader } from '../modules/dataLoader';
+import ApolloClient from './ApolloClient';
+import { PortalProvider } from 'react-ui/build/Portal/Portal';
+import Router from './Router';
 
 const App = () => {
     return (
         <ApolloProvider client={ApolloClient}>
             <ThemeProvider theme={defaultTheme}>
-                <React.Fragment>
-                    <GlobalStyle />
-                    <DataLoader>
-                        <Router />
-                    </DataLoader>
-                </React.Fragment>
+                <PortalProvider>
+                    <React.Fragment>
+                        <GlobalStyle />
+                        <DataLoader>
+                            <Router />
+                        </DataLoader>
+                    </React.Fragment>
+                </PortalProvider>
             </ThemeProvider>
         </ApolloProvider>
     );
