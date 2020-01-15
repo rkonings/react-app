44e918214fc9724d929220aa714541d69e9680a3
rkonings <randy@randykonings.nl>
2020-01-15 12:57:59 +0100

    fix lint error

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,21 +2,66 @@ import React from 'react';
 // import { NavLink } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import { Navigation, NavigationItem } from 'react-ui/build/Navigation';
-import { Home, Inbox, Clients, Agenda, ProjectManagement, Invoices, TimeManagement } from 'react-ui/build/Icon';
+import {
+    Home,
+    Inbox,
+    Clients,
+    Agenda,
+    ProjectManagement,
+    Invoices,
+    TimeManagement,
+} from 'react-ui/build/Icon';
 
 export default () => {
-
-	const history = useHistory();
-	return (
-		<Navigation>
-			<NavigationItem onClick={() => history.push('/')} icon={<Home />}>Dashboard</NavigationItem>
-			<NavigationItem onClick={() => history.push('/settings')} icon={<Home />}>Settings</NavigationItem>
-			<NavigationItem onClick={() => history.push('/inbox')} icon={<Inbox />}>Inbox</NavigationItem>
-			<NavigationItem onClick={() => history.push('/clients')} icon={<Clients />}>Clients</NavigationItem>
-			<NavigationItem onClick={() => history.push('/agenda')} isActive={true} icon={<Agenda />}>Agenda</NavigationItem>
-			<NavigationItem onClick={() => history.push('/project-management')} icon={<ProjectManagement />}>Project management</NavigationItem>
-			<NavigationItem onClick={() => history.push('/invoice')} icon={<Invoices />}>Invoices</NavigationItem>
-			<NavigationItem onClick={() => history.push('/time-management')} icon={<TimeManagement />}>Time management</NavigationItem>
-		</Navigation>
-	);
+    const history = useHistory();
+    return (
+        <Navigation>
+            <NavigationItem onClick={() => history.push('/')} icon={<Home />}>
+                Dashboard
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/settings')}
+                icon={<Home />}
+            >
+                Settings
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/inbox')}
+                icon={<Inbox />}
+            >
+                Inbox
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/clients')}
+                icon={<Clients />}
+            >
+                Clients
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/agenda')}
+                isActive={true}
+                icon={<Agenda />}
+            >
+                Agenda
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/project-management')}
+                icon={<ProjectManagement />}
+            >
+                Project management
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/invoice')}
+                icon={<Invoices />}
+            >
+                Invoices
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/time-management')}
+                icon={<TimeManagement />}
+            >
+                Time management
+            </NavigationItem>
+        </Navigation>
+    );
 };
