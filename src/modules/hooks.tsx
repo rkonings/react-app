854a373ca95d23787539c23a7d918251a52b4ee2
rkonings <randy@randykonings.nl>
2020-01-15 09:42:25 +0100

    update typings

--- src/modules/hooks.tsx
+++ src/modules/hooks.tsx
@@ -184,9 +184,9 @@ export type QueryClientArgs = {
 
 
 export type QueryClientsArgs = {
-  sort?: Maybe<SortInput>,
   type?: Maybe<Array<Maybe<Scalars['String']>>>,
-  city?: Maybe<Array<Maybe<Scalars['String']>>>
+  city?: Maybe<Array<Maybe<Scalars['String']>>>,
+  sort?: Maybe<SortInput>
 };
 
 
