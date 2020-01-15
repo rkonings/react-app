a2a8ad5c951e87adc015235659b100d010d44c76
rkonings <randy@randykonings.nl>
2020-01-15 13:11:59 +0100

    add useRouteMatch to handle isActive state

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { NavLink } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 import { Navigation, NavigationItem } from 'react-ui/build/Navigation';
 import {
     Home,
@@ -14,51 +14,64 @@ import {
 
 export default () => {
     const history = useHistory();
+
     return (
         <Navigation>
-            <NavigationItem onClick={() => history.push('/')} icon={<Home />}>
+            <NavigationItem
+                onClick={() => history.push('/')}
+                isActive={!!useRouteMatch({ path: '/', exact: true })}
+                icon={<Home />}
+            >
                 Dashboard
             </NavigationItem>
             <NavigationItem
                 onClick={() => history.push('/settings')}
                 icon={<Home />}
+                isActive={!!useRouteMatch('/settings')}
             >
                 Settings
             </NavigationItem>
             <NavigationItem
                 onClick={() => history.push('/inbox')}
                 icon={<Inbox />}
+                isActive={!!useRouteMatch('/inbox')}
             >
                 Inbox
             </NavigationItem>
             <NavigationItem
                 onClick={() => history.push('/clients')}
                 icon={<Clients />}
+                isActive={
+                    !!useRouteMatch('/clients') || !!useRouteMatch('/client')
+                }
             >
                 Clients
             </NavigationItem>
             <NavigationItem
                 onClick={() => history.push('/agenda')}
-                isActive={true}
                 icon={<Agenda />}
+                isActive={!!useRouteMatch('/agenda')}
             >
                 Agenda
             </NavigationItem>
             <NavigationItem
                 onClick={() => history.push('/project-management')}
                 icon={<ProjectManagement />}
+                isActive={!!useRouteMatch('/project-management')}
             >
                 Project management
             </NavigationItem>
             <NavigationItem
                 onClick={() => history.push('/invoice')}
                 icon={<Invoices />}
+                isActive={!!useRouteMatch('/invoice')}
             >
                 Invoices
             </NavigationItem>
             <NavigationItem
                 onClick={() => history.push('/time-management')}
                 icon={<TimeManagement />}
+                isActive={!!useRouteMatch('/time-management')}
             >
                 Time management
             </NavigationItem>
