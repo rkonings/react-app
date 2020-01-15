dfed1a14914961444449bd043104f6eef8bd484c
rkonings <41450820+rkonings@users.noreply.github.com>
2020-01-15 13:13:27 +0100

    feat(navigation): add isActive functionality to navigation items (#46)
    * fix lint error
    
    * add useRouteMatch to handle isActive state

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,22 +1,80 @@
 import React from 'react';
 // import { NavLink } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 import { Navigation, NavigationItem } from 'react-ui/build/Navigation';
-import { Home, Inbox, Clients, Agenda, ProjectManagement, Invoices, TimeManagement } from 'react-ui/build/Icon';
+import {
+    Home,
+    Inbox,
+    Clients,
+    Agenda,
+    ProjectManagement,
+    Invoices,
+    TimeManagement,
+} from 'react-ui/build/Icon';
 
 export default () => {
+    const history = useHistory();
 
-	const history = useHistory();
-	return (
-		<Navigation>
-			<NavigationItem onClick={() => history.push('/')} icon={<Home />}>Dashboard</NavigationItem>
-			<NavigationItem onClick={() => history.push('/settings')} icon={<Home />}>Settings</NavigationItem>
-			<NavigationItem onClick={() => history.push('/inbox')} icon={<Inbox />}>Inbox</NavigationItem>
-			<NavigationItem onClick={() => history.push('/clients')} icon={<Clients />}>Clients</NavigationItem>
-			<NavigationItem onClick={() => history.push('/agenda')} isActive={true} icon={<Agenda />}>Agenda</NavigationItem>
-			<NavigationItem onClick={() => history.push('/project-management')} icon={<ProjectManagement />}>Project management</NavigationItem>
-			<NavigationItem onClick={() => history.push('/invoice')} icon={<Invoices />}>Invoices</NavigationItem>
-			<NavigationItem onClick={() => history.push('/time-management')} icon={<TimeManagement />}>Time management</NavigationItem>
-		</Navigation>
-	);
+    return (
+        <Navigation>
+            <NavigationItem
+                onClick={() => history.push('/')}
+                isActive={!!useRouteMatch({ path: '/', exact: true })}
+                icon={<Home />}
+            >
+                Dashboard
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/settings')}
+                icon={<Home />}
+                isActive={!!useRouteMatch('/settings')}
+            >
+                Settings
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/inbox')}
+                icon={<Inbox />}
+                isActive={!!useRouteMatch('/inbox')}
+            >
+                Inbox
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/clients')}
+                icon={<Clients />}
+                isActive={
+                    !!useRouteMatch('/clients') || !!useRouteMatch('/client')
+                }
+            >
+                Clients
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/agenda')}
+                icon={<Agenda />}
+                isActive={!!useRouteMatch('/agenda')}
+            >
+                Agenda
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/project-management')}
+                icon={<ProjectManagement />}
+                isActive={!!useRouteMatch('/project-management')}
+            >
+                Project management
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/invoice')}
+                icon={<Invoices />}
+                isActive={!!useRouteMatch('/invoice')}
+            >
+                Invoices
+            </NavigationItem>
+            <NavigationItem
+                onClick={() => history.push('/time-management')}
+                icon={<TimeManagement />}
+                isActive={!!useRouteMatch('/time-management')}
+            >
+                Time management
+            </NavigationItem>
+        </Navigation>
+    );
 };
