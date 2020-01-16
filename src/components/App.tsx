35c58ad32aa5e363af30cc96514832bdd9fd4f5e
rkonings <randy@randykonings.nl>
2020-01-16 10:57:54 +0100

    add AuthProvider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,24 +3,30 @@ import React from 'react';
 import GlobalStyle from 'react-ui/build/GlobalStyle';
 import defaultTheme from 'react-ui/build/themes/default';
 import ThemeProvider from 'react-ui/build/themes/Provider';
-import { DataLoader } from '../modules/dataLoader';
 import ApolloClient from './ApolloClient';
 import { PortalProvider } from 'react-ui/build/Portal/Portal';
 import Router from './Router';
+import { AuthProvider, Authenticated } from '../modules/auth';
+import Login from '../components/Login';
+import { DataLoader } from '../modules/dataLoader';
 
 const App = () => {
     return (
         <ApolloProvider client={ApolloClient}>
-            <ThemeProvider theme={defaultTheme}>
-                <PortalProvider>
-                    <React.Fragment>
-                        <GlobalStyle />
+            <AuthProvider>
+                <ThemeProvider theme={defaultTheme}>
+                    <Authenticated login={<Login />}>
                         <DataLoader>
-                            <Router />
+                            <PortalProvider>
+                                <React.Fragment>
+                                    <GlobalStyle />
+                                    <Router />
+                                </React.Fragment>
+                            </PortalProvider>
                         </DataLoader>
-                    </React.Fragment>
-                </PortalProvider>
-            </ThemeProvider>
+                    </Authenticated>
+                </ThemeProvider>
+            </AuthProvider>
         </ApolloProvider>
     );
 };
