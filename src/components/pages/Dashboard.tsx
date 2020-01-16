b9d6f3ba48faf4873f0a64f29b69cfd39aa9057c
rkonings <randy@randykonings.nl>
2020-01-16 10:57:35 +0100

    add logout button

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Navigation from '../Navigation';
 import Basic from 'react-ui/build/Layout/Basic';
+import { useAuth } from '../../modules/auth';
 
 export default () => {
-	return (
-		<React.Fragment>
-			<Basic
-				left={<Navigation/>}
-				pageTitle="Dashboard"
-			>
-				<div>Dashboard</div>
-			</Basic>
-		</React.Fragment>
-	);
+    const { logout } = useAuth();
+
+    return (
+        <React.Fragment>
+            <Basic left={<Navigation />} pageTitle="Dashboard">
+                <div>Dashboard</div>
+                <button
+                    onClick={() => {
+                        logout();
+                    }}
+                >
+                    Logout
+                </button>
+            </Basic>
+        </React.Fragment>
+    );
 };
